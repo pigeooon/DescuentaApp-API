dca4a1634bb106dc515fb8d623288630f66391b1
combatdev <54486248+combatdev@users.noreply.github.com>
2022-09-11 19:42:47 -0300

    bank type

---
+++ src/types/bank.type.ts
@@ -0,0 +1,8 @@
+export type Bank = { 
+    name: string;
+    url: string;
+    discount_name_selector: string; 
+    discount_img_selector: string;
+    discount_description_selector: string;
+    discount_details_url_selector: string;
+}
--- src/types/discounts_repository.type.ts
+++
@@ -1,7 +0,0 @@
-export type DiscountsRepository = { 
-    url: string;
-    discount_name_path: string;
-    discount_img_path: string;
-    discount_description_path: string;
-    discount_details_url_path: string;
-}
