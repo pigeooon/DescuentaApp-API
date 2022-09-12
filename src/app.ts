359f7b7947c1ac0d611b2923d4349576373166c3
combatdev <54486248+combatdev@users.noreply.github.com>
2022-09-11 23:32:33 -0300

    mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-import { Scraper } from "./models/scraper.model";
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+mongoose.connect(String(process.env.DB_CONN_STRING)).then(
+    () => { console.log("✅ Connected successfully to MongoDB."); },
+).catch((err) => {
+    console.log("❌ MongoDB connection error.");
+    console.error(err);
+});
+
+/*import { Scraper } from "./models/scraper.model";
 import { Bank } from "./types/bank.type";
 
 const banks: Bank[] = [
     {
         name: "Scotiabank",
         url: "https://www.scotiaclub.cl/scclubfront/categoria/mundos/descuentos",
+        img_source_url: "https://www.scotiaclub.cl/",
         discount_name_selector: "#row-descuentos > .descuento > .card > .card-body > .scotia-headline",
         discount_img_selector: "#row-descuentos > .descuento > .card > img",
         discount_description_selector: "#row-descuentos > .descuento > .card > .card-body > .mb-1",
@@ -13,4 +24,4 @@ const banks: Bank[] = [
 ];
 
 const scraper = new Scraper(banks);
-scraper.scrap();
+scraper.scrap();*/
