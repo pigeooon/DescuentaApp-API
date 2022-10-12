953b372bf04e18daf80a3a575de89b44bc272314
combatdev <54486248+combatdev@users.noreply.github.com>
2022-10-11 23:16:59 -0300

    update scrap service

--- src/services/scraper.service.ts
+++ src/services/scraper.service.ts
@@ -6,7 +6,7 @@ export class ScraperService {
     constructor() {
     }
 
-    async scrap(bank: BankType): Promise<DiscountType[]> {
+    public async scrap(bank: BankType): Promise<DiscountType[]> {
         const browser: Browser = await puppeteer.launch();
         const page: Page = await browser.newPage();
 
@@ -14,26 +14,48 @@ export class ScraperService {
             waitUntil:["load", "domcontentloaded", "networkidle0", "networkidle2"]
         });
     
-        //falta manejar cuando $$eval no encuentra el item (retorna una promesa)
-        const discounts_name_vector = await page.$$eval(bank.discount_name_selector, item => item.map((item) => item.innerHTML));
-        const discounts_img_vector = await page.$$eval(bank.discount_img_selector, item => item.map((item) => item.getAttribute('src')));
-        const discounts_description_vector = await page.$$eval(bank.discount_description_selector, item => item.map((item) => item.innerHTML));
-        const discounts_details_url_vector = await page.$$eval(bank.discount_details_url_selector, item => item.map((item) => item.innerHTML));
+        //llamadas a distintos métodos de selección
+        const discounts_name_vector = await this.scrapPlainText(page, bank.discount_name_selector);
+        const discounts_img_vector = await this.scrapImgUrl(page, bank.discount_img_url_selector, bank.img_source_url);
+        const discounts_description_vector = await this.scrapPlainText(page, bank.discount_description_selector);
+        const discounts_details_vector = await this.scrapPlainText(page, bank.discount_details_selector || " ");
+        const discounts_location_vector = await this.scrapPlainText(page, bank.discount_location_selector || " ");
+        const discounts_date_vector = await this.scrapPlainText(page, bank.discount_date_selector || " ");
 
+        //cerramos la instancia del browser
         await browser.close();
 
+        //construimos el array de DiscountTypes
         const discountsArray: DiscountType[] = [];
-        
         discounts_name_vector.map((_value, index) => {
             discountsArray.push({
-                bank: bank.name,
                 name: discounts_name_vector[index],
-                img: bank.img_source_url + discounts_img_vector[index],
+                img_url: discounts_img_vector[index],
                 description: discounts_description_vector[index],
-                details_url: '#', // no todos los casos tienen url, algunos tienen el detalle en js
+                details: discounts_details_vector[index],
+                bank: bank.name,
+                category: ' ',
+                location: discounts_location_vector[index],
+                date: discounts_date_vector[index],
             });
 
         });
         return discountsArray;
     }
+
+    private async scrapPlainText(page: Page, selector: string) {
+        const results = await page.$$eval(selector, item => item.map((item) => item.innerHTML));
+        return results;
+    } 
+
+    private async scrapImgUrl(page: Page, selector: string, base_url: string) {
+        const results = await page.$$eval(selector, item => item.map((item) => item.getAttribute('src')));
+        const formattedResults: any[] = [];
+
+        results.map((value, _index) => {
+            formattedResults.push((base_url + value).replace(/ /g, '%20')); //cambiar espacios por %20
+        });
+        return formattedResults;
+    }
+
 }
