e04d2224e7b73faa2311fe3e60205dbc8c6d216f
combatdev <54486248+combatdev@users.noreply.github.com>
2022-10-13 17:37:38 -0300

    discount categories enum

---
+++ src/utils/enums.ts
@@ -0,0 +1,16 @@
+enum DiscountCategories {
+    ALIMENTOS = "Alimentos",
+    DEPORTES = "Deportes",
+    EDUCACION = "Educación",
+    ENTRETENCION = "Entretención",
+    HOGAR = "Hogar",
+    HOTELES = "Hoteles",
+    MASCOTAS = "Mascotas",
+    ROPA = "Ropa",
+    SALUD_Y_BELLEZA = "Salud y belleza",
+    SUSTENTABLE = "Sustentable",
+    TECNOLOGIA = "Tecnología",
+    TRANSPORTE = "Transporte",
+    VIAJES = "Viajes",
+    OTROS = "Otros"
+}
