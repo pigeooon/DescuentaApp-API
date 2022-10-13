df95d9dbd5d430a342054a6d3486edd48047d7c7
combatdev <54486248+combatdev@users.noreply.github.com>
2022-10-13 16:30:55 -0300

    Update scraper.service.ts

--- src/services/scraper.service.ts
+++ src/services/scraper.service.ts
@@ -18,7 +18,6 @@ export class ScraperService {
         const discounts_name_vector = await this.scrapPlainText(page, bank.discount_name_selector);
         const discounts_img_vector = await this.scrapImgUrl(page, bank.discount_img_url_selector, bank.img_source_url);
         const discounts_description_vector = await this.scrapPlainText(page, bank.discount_description_selector);
-        const discounts_location_vector = await this.scrapPlainText(page, bank.discount_location_selector || " ");
         const discounts_date_vector = await this.scrapPlainText(page, bank.discount_date_selector || " ");
 
         //cerramos la instancia del browser
@@ -33,7 +32,6 @@ export class ScraperService {
                 description: discounts_description_vector[index],
                 bank: bank.name,
                 category: ' ',
-                location: discounts_location_vector[index],
                 date: discounts_date_vector[index],
             });
 
