60bbc461ad6b1c9c7cd1565f358f6753cf69e9cc
combatdev <54486248+combatdev@users.noreply.github.com>
2022-11-14 13:52:13 -0300

    name and preferences account attributes

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
 export interface IAccount {
+    name: string;
     email: string;
     password: string;
     administrator: boolean;
+    preferences: [];
 }
 
 export type AccountDocument = mongoose.Document & {
+    name: string;
     email: string;
     password: string;  
     administrator: boolean;
+    preferences: [];
 };
--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import { AccountDocument } from "../interfaces/account.interface";
 
-const bankSchema = new mongoose.Schema<AccountDocument> (
+const accountSchema = new mongoose.Schema<AccountDocument> (
     {
+        name: String,
         email: String,
         password: String,
         administrator: Boolean,
+        preferences: []   
     }
 );
 
-export const Account = mongoose.model<AccountDocument>("Account", bankSchema);
+export const Account = mongoose.model<AccountDocument>("Account", accountSchema);
