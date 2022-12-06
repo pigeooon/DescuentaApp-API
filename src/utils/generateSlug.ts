f65ad10bbc2acaf893936f6d2e39df610f106b4a
combatdev <54486248+combatdev@users.noreply.github.com>
2022-12-06 11:46:10 -0300

    remove accents function

--- src/utils/generateSlug.ts
+++ src/utils/generateSlug.ts
@@ -1,18 +1,10 @@
-export const generateSlug = (bank: string, name: string) => {
+import { removeAccents } from "./removeExtraCharacters";
 
+export const generateSlug = (bank: string, name: string) => {
     let slug = (bank + " " + name).replace(/^\s+|\s+$/g, ''); // trim
-    slug = slug.toLowerCase();
-  
-    // remove accents, swap ñ for n, etc
-    const from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆĞÍÌÎÏİŇÑÓÖÒÔÕØŘŔŠŞŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇğíìîïıňñóöòôõøðřŕšşťúůüùûýÿžþÞĐđßÆa";
-    const to   = "AAAAAACCCDEEEEEEEEGIIIIINNOOOOOORRSSTUUUUUYYZaaaaaacccdeeeeeeeegiiiiinnooooooorrsstuuuuuyyzbBDdBAa";
-    for (var i = 0, l = from.length; i < l; i++) {
-      slug = slug.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-    }
-  
+    slug = removeAccents(slug.toLowerCase());
     slug = slug.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
              .replace(/\s+/g, '-') // collapse whitespace and replace by -
              .replace(/-+/g, '-'); // collapse dashes
-  
     return slug;
 }
--- src/utils/removeExtraCharacters.ts
+++ src/utils/removeExtraCharacters.ts
@@ -3,3 +3,13 @@ export const removeExtraCharacters = (text: string) => {
     cleanText.replace("&amp;", "&");
     return cleanText;
 }
+
+export const removeAccents = (text: string) => {
+    let textWithoutAccents = text;
+    const from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆĞÍÌÎÏİŇÑÓÖÒÔÕØŘŔŠŞŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇğíìîïıňñóöòôõøðřŕšşťúůüùûýÿžþÞĐđßÆa";
+    const to   = "AAAAAACCCDEEEEEEEEGIIIIINNOOOOOORRSSTUUUUUYYZaaaaaacccdeeeeeeeegiiiiinnooooooorrsstuuuuuyyzbBDdBAa";
+    for (let i = 0, l = from.length; i < l; i++) {
+        textWithoutAccents = textWithoutAccents.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+    return textWithoutAccents;
+}
